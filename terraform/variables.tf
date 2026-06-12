9fbc9edcb7ba4e4932285234e28e9cc9cbc7ef29
Mike Akonya <mikeakonya87@gmail.com>
2026-06-12 12:03:59 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "location" {
 variable "app_name" {
   description = "Name of the Azure App Service (must be globally unique)"
   type        = string
-  default     = "hello-world-python-app-2026"
+  default     = "hello-world-python-app-2026-makonya"
 }
 
 variable "tags" {
